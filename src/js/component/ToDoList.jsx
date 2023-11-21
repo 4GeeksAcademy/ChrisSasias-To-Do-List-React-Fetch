ce2d2fe3078efffc6ec6c2cd4b12d9648681477f
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-11-21 04:48:17 +0000

    inclusion put method

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -4,33 +4,52 @@ const ToDoList = () => {
     const [task, setTask] = useState({ "label": "", "done": false });
     const [tasks, setTasks] = useState([]);
 
+    const Url = "https://playground.4geeks.com/apis/fake/todos/user/ChrisSasias";
+
     const GetPast = async () => {
         try {
-            let response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/ChrisSasias");
+            let response = await fetch(Url);
             if (response.ok) {
                 let data = await response.json()
-
                 setTasks(data);
-
             }
         } catch(error) {
-
-            
             console.log(error);
-
         }
-
     }
 
-    useEffect(()=>{GetPast()}, []);
+    const updateData = async () => {
+        try {
+            let response = await fetch(Url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(tasks)
+            });
 
-    // const addTask = () => {
-    //     setTasks([...tasks, task]);
-    //     setTask("");
-    // };
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        } catch(error) {
+            console.log("Hubo un error: ", error);
+        }
+    }
+
+    useEffect(() => {
+        GetPast();
+    }, []);
+
+    const addTask = () => {
+        setTasks([...tasks, task]);
+        setTask({ "label": "", "done": false });
+        updateData();
+    };
+
     const clear = (taskToDelete) => {
         const updatedTasks = tasks.filter((task) => task !== taskToDelete);
         setTasks(updatedTasks);
+        updateData();
     };
 
     return (
@@ -40,17 +59,17 @@ const ToDoList = () => {
             <input
                 type="text"
                 value={task.label}
-                onChange={(event) => setTask(event.target.value)}
+                onChange={(event) => setTask({ "label": event.target.value, "done": false })}
             />
 
-            <button className="Button btn btn-success " >
+            <button className="Button btn btn-success " onClick={addTask}>
                 Add
             </button>
             <ul>
                 {tasks.map((item, index) => (
                     <li key={index}>
                         {item.label}
-                        <span className="delete" onClick={() => clear(item.label)}>
+                        <span className="delete" onClick={() => clear(item)}>
                             🗑️{" "}
                         </span>
                     </li>

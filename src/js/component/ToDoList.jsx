509100829ba10bb1c17f786f2a2128420884e81e
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-11-24 03:27:05 +0000

    correciones realiadas

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,82 +1,127 @@
 import React, { useState, useEffect } from "react";
 
 const ToDoList = () => {
-    const [task, setTask] = useState({ "label": "", "done": false });
-    const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [highlightedTask, setHighlightedTask] = useState(null);
 
-    const Url = "https://playground.4geeks.com/apis/fake/todos/user/ChrisSasias";
+  useEffect(() => {
+    createUser("https://playground.4geeks.com/apis/fake/todos/user/ChrisSasias", []);
+    fetchTasks();
+  }, []);
 
-    const GetPast = async () => {
-        try {
-            let response = await fetch(Url);
-            if (response.ok) {
-                let data = await response.json()
-                setTasks(data);
-            }
-        } catch(error) {
-            console.log(error);
-        }
+  useEffect(() => {
+    updateList();
+  }, [tasks]);
+
+  const createUser = async (url, body) => {
+
+    try {
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error al agregar tarea! Status: ${response.status}`);
+      }
+
+      console.log(response);
+
+    } catch (error) {
+      console.error("Error al agregar tarea:", error);
     }
+  };
 
-    const updateData = async () => {
-        try {
-            let response = await fetch(Url, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(tasks)
-            });
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-        } catch(error) {
-            console.log("Hubo un error: ", error);
-        }
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/ChrisSasias");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.error("Error al obtener la lista de tareas:", error);
     }
+  };
 
-    useEffect(() => {
-        GetPast();
-    }, []);
+  const addTask = () => {
 
-    const addTask = () => {
-        setTasks([...tasks, task]);
-        setTask({ "label": "", "done": false });
-        updateData();
-    };
+    setTasks([...tasks, { label: task, done: false }]);
 
-    const clear = (taskToDelete) => {
-        const updatedTasks = tasks.filter((task) => task !== taskToDelete);
-        setTasks(updatedTasks);
-        updateData();
-    };
+  }
 
-    return (
-        <div className="container">
-            <h1>List Of Tasks</h1>
 
-            <input
-                type="text"
-                value={task.label}
-                onChange={(event) => setTask({ "label": event.target.value, "done": false })}
-            />
+  const removeTask = (taskToDelete) => {
 
-            <button className="Button btn btn-success " onClick={addTask}>
-                Add
-            </button>
-            <ul>
-                {tasks.map((item, index) => (
-                    <li key={index}>
-                        {item.label}
-                        <span className="delete" onClick={() => clear(item)}>
-                            🗑️{" "}
-                        </span>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    setTasks(tasks.filter((t) => t.label !== taskToDelete.label));
+
+  }
+
+  const updateList = async () => {
+    try {
+
+      const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/ChrisSasias", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(tasks),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error al agregar tarea! Status: ${response.status}`);
+      }
+      setTask("");
+    } catch (error) {
+      console.error("Error al agregar tarea:", error);
+    }
+  };
+
+
+  return (
+    <div className="container">
+      <h1>List Of Tasks</h1>
+
+      <input
+        type="text"
+        value={task}
+        onChange={(event) => setTask(event.target.value)}
+        onKeyDown={(event) => event.key === "Enter" && task.trim() !== "" && addTask()}
+      />
+
+      <button className="Button btn btn-success " onClick={addTask}>
+        Add
+      </button>
+
+      {tasks.length === 0 ? (
+        <p className="msg">No tasks entered.</p>
+      ) : (
+        <ul>
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              onMouseOver={() => setHighlightedTask(index)}
+            >
+              {task.label}
+              {(highlightedTask === index || highlightedTask === "delete") && (
+                <span className="delete" onClick={() => removeTask(task)}>
+                  🗑️{" "}
+                </span>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default ToDoList;
